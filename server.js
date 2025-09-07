c11a32a9bb93c74644d9208680a2ad2099ee97c1
Younes Bachouche <younesbachouche1@gmail.com>
2025-09-07 07:15:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(PORT, () =>
-  console.log(`iOS-compatible proxy running on http://localhost:${PORT}`)
+console.log(`iOS-compatible proxy running on https://m3u8proxy-2dwu.onrender.com`);
 );
